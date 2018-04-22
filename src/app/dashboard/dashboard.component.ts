2b258dde03553508256fadeeb3cd7a23d290d34e
Prakash Malakar <pramalakar.2010@gmail.com>
2018-04-22 16:49:38 +1000

    Added Articles Chart Report

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import {DataService} from '../../providers/data-service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  chart;
+  showLoading = false;
+  datasets;
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.getData();
+  }
+  getData() {
+    this.showLoading = true;
+    this.dataService.execute('post', '/api/Article/ArticleReport', {}).subscribe((val) => {
+      this.showLoading = false;
+      const data = val.map(res => res.count);
+      const label = val.map(res => res.date);
+      this.datasets = [{
+        label: 'Articles',
+        borderColor: '#82B1FF',
+        backgroundColor: '#82B1FF',
+        data: data
+      }]
+      this.lineChart(label, data);
+    });
   }
 
+  lineChart(label, data) {
+    // Set the chart colors
+
+    // Get the canvas element
+    const ctx = document.getElementById('line-chart');
+
+    const lineChartData = {
+      labels: label,
+      tooltips: {
+        enabled: true,
+        position: 'average'
+      },
+      datasets: this.datasets
+    };
+
+    const line = new Chart(ctx, {
+      type: 'line',
+      data: lineChartData,
+      options: {
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
 }
